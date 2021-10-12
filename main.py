70a76173eda4393631c16c58ed1db97d90b2d4f2
updonion <87442471+updonion@users.noreply.github.com>
2021-10-12 22:59:04 +0300

    One by one
    Now I have to type domain names one by one. But I want to parse coma separated values from terminal input and run several tasks simultaneously.

---
+++ main.py
@@ -0,0 +1,30 @@
+import requests
+from requests.structures import CaseInsensitiveDict
+from bs4 import BeautifulSoup
+
+def getPage(domain_name):
+    url = "https://www.registry.in/domain-search"
+
+    headers = CaseInsensitiveDict()
+    headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0"
+    headers["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
+    headers["Accept-Language"] = "en-US,en;q=0.5"
+    headers["Content-Type"] = "application/x-www-form-urlencoded"
+    headers["Origin"] = "null"
+    headers["Connection"] = "keep-alive"
+    headers["Upgrade-Insecure-Requests"] = "1"
+
+    data = f"domainUniquelang=english&domain_name={domain_name}&tld=in&domain=&referer=https%3A%2F%2Fwww.registry.in%2Fdomain-search&is_domain_available=&op=SEARCH"
+    resp = requests.post(url, headers=headers, data=data)
+
+    return(resp.text)
+
+def pageParser(domain_name):
+    soup = BeautifulSoup(getPage(domain_name), 'html.parser')
+    return("Not available" if soup.select(".text-center-align") else "Available")
+
+while(True):
+    domain_name = input("Type the name without .in: ")
+    if(domain_name == "0"):
+        break
+    print(pageParser(domain_name))
